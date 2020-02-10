41dc68e4e5480da4083f874329e51f74d79ed9d5
RizwanaParveen <rizwanaparveen64@gmail.com>
2020-02-10 16:22:19 +0530

    Update local_exec.tf

--- 04-Terraform/03-Ansible-Deploy/local_exec.tf
+++ 04-Terraform/03-Ansible-Deploy/local_exec.tf
@@ -6,24 +6,73 @@ terraform {
 }
 
 provider "aws" {
-  region = "us-west-2"
+  region = var.region
+}
+resource "aws_vpc" "test-env" {
+  cidr_block = var.cidr_block
+  enable_dns_hostnames = true
+  enable_dns_support = true
+}
+resource "aws_subnet" "subnet-uno" {
+  cidr_block = "${cidrsubnet(aws_vpc.test-env.cidr_block, 3, 1)}"
+  vpc_id = "${aws_vpc.test-env.id}"
+  availability_zone =var.zone
+}
+resource "aws_security_group" "ingress-all-test" {
+name = "allow-all-sg"
+vpc_id = "${aws_vpc.test-env.id}"
+ingress {
+    cidr_blocks = [
+      "0.0.0.0/0"
+    ]
+from_port = var.port
+    to_port = var.port
+    protocol = "tcp"
+  }
+// Terraform removes the default rule
+  egress {
+   from_port = 0
+   to_port = 0
+   protocol = "-1"
+   cidr_blocks = ["0.0.0.0/0"]
+ }
+}
+resource "aws_instance" "test-ec2-instance" {
+  ami = "${var.ami_id}"
+  instance_type = "t2.micro"
+  key_name = "${var.ami_key_pair_name}"
+  security_groups = ["${aws_security_group.ingress-all-test.id}"]
+subnet_id = "${aws_subnet.subnet-uno.id}"
+}
+
+resource "aws_eip" "ip-test-env" {
+  instance = "${aws_instance.test-ec2-instance.id}"
+  vpc      = true
+}
+resource "aws_internet_gateway" "test-env-gw" {
+  vpc_id = "${aws_vpc.test-env.id}"
   
 }
-
-resource "aws_instance" "backend" {
-  ami                    = "ami-0994c095691a46fb5"
-  instance_type          = "t2.micro"
-  key_name               = "${var.key_name}"
-  vpc_security_group_ids = ["${var.sg-id}"]
+resource "aws_route_table" "route-table-test-env" {
+  vpc_id = "${aws_vpc.test-env.id}"
+route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_internet_gateway.test-env-gw.id}"
+  }
 
 }
+resource "aws_route_table_association" "subnet-association" {
+  subnet_id      = "${aws_subnet.subnet-uno.id}"
+  route_table_id = "${aws_route_table.route-table-test-env.id}"
+}
+
 
 resource "null_resource" "remote-exec-1" {
     connection {
     user        = "ubuntu"
     type        = "ssh"
     private_key = "${file(var.pvt_key)}"
-    host        = "${aws_instance.backend.public_ip}"
+    host        = "${aws_instance.test-ec2-instance.public_ip}"
   }
 
   provisioner "remote-exec" {
@@ -41,7 +90,7 @@ provisioner "local-exec" {
         > jenkins-ci.ini;
         echo "[jenkins-ci]"| tee -a jenkins-ci.ini;
         export ANSIBLE_HOST_KEY_CHECKING=False;
-        echo "${aws_instance.backend.public_ip}" | tee -a jenkins-ci.ini;
+        echo "${aws_instance.test-ec2-instance.public_ip}" | tee -a jenkins-ci.ini;
         ansible-playbook  --key=${var.pvt_key} -i jenkins-ci.ini ./ansible/04-Tomcat/web-playbook.yaml -u ubuntu -v
     EOT
 }
